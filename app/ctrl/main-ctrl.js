8ea3593b83953204e6dd0491cc1d8e88fee0d744
Cahlan Sharp <cahlan@gmail.com>
2014-10-08 21:24:19 -0600

    adds stuff

--- app/ctrl/main-ctrl.js
+++ app/ctrl/main-ctrl.js
@@ -2,11 +2,11 @@ angular.module('SiriClient').controller('MainCtrl', function($scope, SiriService
   $scope.messageHistory = [];
   $scope.sendMessage = function() {
     console.log('sendMessage');
-    $scope.messageHistory.push($scope.message);
+    $scope.messageHistory.push({text:$scope.message});
     $scope.message = '';
-    SiriService.send($scope.message).then(function(reply) {
-      console.log('reply', reply);
-      $scope.messageHistory.push('>> '+reply);
+    SiriService.send($scope.message).then(function(response) {
+      var reply = response.data.message;
+      $scope.messageHistory.push({text: '>> '+reply});
     });
   };
 });
